5c9f863670b5effa31512963dbea4a60c82b338d
burhanHere <burhanburewala@gmail.com>
2024-07-05 14:10:36 +0500

    second commit

--- obj/Debug/net8.0/UnderstandingDelegates.AssemblyInfo.cs
+++ obj/Debug/net8.0/UnderstandingDelegates.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnderstandingDelegates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6669ef84e31fce8af0fd84b4d4bea2bd28b846dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b01dd73a437706997c662caac0a9201891244f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnderstandingDelegates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnderstandingDelegates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
